f6df61ff746aa7a2c3116a9099d9ae0017a2e54c
Andrii Dovhaliuk <andy87@live.ru>
2022-10-09 12:07:07 +0400

    Update logic to load home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello from index.js")
+import './style.css';
+import homeContentDiv from './modules/home.js';
+
+const contentDiv = document.getElementById('content');
+let homeBtn = document.getElementById('home-button');
+
+function loadPage(page) {
+
+    contentDiv.remove(contentDiv.firstChild);
+    let element = page;
+
+    return element;
+}
+
+homeBtn.addEventListener('click', openHomePage);
+
+contentDiv.appendChild(component());
+
+function openHomePage() {
+    loadPage(homeContentDiv());
+}
+
+
+
