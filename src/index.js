6c38886258669a0e1dd22b52e6d357543531a82a
Andrii Dovhaliuk <andy87@live.ru>
2022-10-09 14:09:21 +0400

    Add logic for opening contact page

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import './style.css';
 import homeContentDiv from './modules/home.js';
 import menuContentDiv from './modules/menu.js';
+import contactContentDiv from './modules/contact.js'
 
 (function () {
     const contentDiv = document.getElementById('content');
     const homeBtn = document.getElementById('home-button');
     const menuBtn = document.getElementById('menu-button');
+    const contactBtn = document.getElementById('contact-button');
 
     homeBtn.addEventListener('click', openHomePage);
     menuBtn.addEventListener('click', openMenuPage);
+    contactBtn.addEventListener('click', openContactPage);
 
     loadPage(homeContentDiv());
 
     function loadPage(page) {
         clearContentDiv();
         contentDiv.appendChild(page);
+    }
+
+    function openContactPage() {
+        loadPage(contactContentDiv());
     }
 
     function openHomePage() {
